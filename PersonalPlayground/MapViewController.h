165be71a3d31c85bef9fe8b815ddd09764abc355
Varga Zolt <sky@Vargas-MacBook-Pro.local>
2017-06-04 23:37:56 +0200

    init view

---
+++ PersonalPlayground/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  PersonalPlayground
+//
+//  Created by Varga Zolt on 6/4/17.
+//  Copyright © 2017 Varga Zolt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+
+
+@end
---
+++ PersonalPlayground/MapViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewController.m
+//  PersonalPlayground
+//
+//  Created by Varga Zolt on 6/4/17.
+//  Copyright © 2017 Varga Zolt. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
